a4e45b7a8459093d7f2d161f81bde8efc344c935
Navid <50656059+naviiidz@users.noreply.github.com>
2023-08-06 17:10:56 -0400

    Update Step2.cpp

--- Step2.cpp
+++ Step2.cpp
@@ -7,7 +7,25 @@
 using namespace std;
 using namespace g2o;
 
-int main() {
+int main(int argc, char** argv) { //First change 
+//Second change**
+int maxIterations;
+string outputFilename;
+string inputFilename;
+CommandArgs arg;
+arg.param("iter", maxIterations, 10,
+          "perform n iterations, if negative consider the gain");
+arg.param("out", outputFilename, "", "output final version of the graph");
+arg.param("in", inputFilename, "", "output final version of the graph");
+
+arg.parseArgs(argc, argv);
+
+cout<<maxIterations<<" is maximum iterations"<<endl;
+cout<<outputFilename<<" is output file name"<<endl;
+
+// **
+
+  
 SparseOptimizer optimizer; // creating an instance of the optimizer
 optimizer.setVerbose(true);
 
