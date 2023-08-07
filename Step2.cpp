cbcad3ff427845283bd49c55d962a30a21e14f38
Navid <50656059+naviiidz@users.noreply.github.com>
2023-08-07 15:13:01 -0400

    Update Step2.cpp

--- Step2.cpp
+++ Step2.cpp
@@ -8,6 +8,7 @@ using namespace std;
 using namespace g2o;
 
 int main(int argc, char** argv) { //First change 
+
 //Second change**
 int maxIterations;
 string outputFilename;
@@ -17,24 +18,18 @@ arg.param("iter", maxIterations, 10,
           "perform n iterations, if negative consider the gain");
 arg.param("out", outputFilename, "", "output final version of the graph");
 arg.param("in", inputFilename, "", "output final version of the graph");
-
 arg.parseArgs(argc, argv);
-
 cout<<maxIterations<<" is maximum iterations"<<endl;
 cout<<outputFilename<<" is output file name"<<endl;
-
 // **
 
-  
 SparseOptimizer optimizer; // creating an instance of the optimizer
 optimizer.setVerbose(true);
-
 // allocate the solver
 g2o::OptimizationAlgorithmProperty solverProperty;
 optimizer.setAlgorithm(
 g2o::OptimizationAlgorithmFactory::instance()->construct("lm_var",
 solverProperty));
-
 }
 
 
